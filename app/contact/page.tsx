54cd0c7acbd2a7c56b0ebbcfaeee40dbc3e28e98
ashraful-commits <ashrfulalam10000@gmail.com>
2023-08-25 08:02:03 +0600

    contact form update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -43,11 +43,11 @@ const Contact = () => {
       <div className="row flex justify-center dark:bg-transparent dark:bg-opacity-50  w-full h-full p-5 items-center ">
         <div className="col flex justify-center flex-col items-center w-full h-full">
           <div className="contact w-[80vw] h-[80vh] md:w-[85vw] md:h-[50vh] lg:w-[60vw] lg:h-[60vh] flex flex-col md:flex-row lg:flex-row">
-            <div className="part1 w-[100%] bg-purple-100 dark:bg-gray-900 dark:bg-opacity-80 z-0 h-[100%]  flex justify-center items-center relative">
+            <div className="part1 w-[100%] bg-purple-100 rounded-tl-xl  lg:rounded-tl-xl rounded-tr-xl md:rounded-tr-none lg:rounded-tr-none md:rounded-tl-xl md:rounded-bl-xl dark:bg-gray-900 dark:bg-opacity-80 z-0 h-[100%]  flex justify-center items-center relative">
               <Image
                 src={chat}
                 alt="contact"
-                className="w-[200px] h-[200px] lg:w-[250px] lg:h-[250px] rounded-xl mt-5 z-10 object-cover absolute "
+                className="w-[150px] h-[150px] lg:w-[250px] lg:h-[250px] rounded-xl mt-5 z-10 object-cover absolute "
               />
               <div className="z-20 absolute top-[10%] flex items-center justify-start flex-col w-full">
                 <p className="flex justify-center items-center gap-5 text-purple-500 text-xl">
@@ -61,13 +61,19 @@ const Contact = () => {
                 </button>
               </div>
             </div>
-            <div className="part2 w-[100%] h-[100%] relative flex flex-col justify-between bg-purple-500 dark:bg-gray-900 dark:bg-opacity-80 lg:p-10 md:p-7 p-5">
+            <div className="part2 w-[100%] h-[100%] relative flex flex-col rounded-bl-xl lg:rounded-bl-none md:rounded-bl-none rounded-br-xl  md:rounded-tr-xl lg:rounded-tr-xl md:rounded-br-xl  lg:rounded-br-xl justify-between bg-purple-500 dark:bg-gray-900 dark:bg-opacity-80 lg:p-10 md:p-7 p-5">
               <span className="absolute w-5 h-5 rounded-full bg-white"></span>
-              <h1 className="text-right text-[45px] leading-10 text-white font-bold font-serif italic">
+              <h1 className="text-right text-[30px] lg:text-[45px] leading-10 text-white md:text-[40px] font-bold font-serif italic">
                 Contact <br />
-                <span className="text-[40px]"> ________Me</span>
+                <span className="text-[25px] lg:text-[30px] md:text-[30px]">
+                  {" "}
+                  ________Me
+                </span>
               </h1>
-              <form action="" className="flex flex-col gap-5 ">
+              <form
+                action=""
+                className="flex flex-col gap-2 lg:gap-5 md:gap-5 "
+              >
                 <input
                   type="text"
                   className=" focus:border-b-2 focus:outline-none  bg-transparent text-sm border-b-2 py-2 placeholder:text-white"
@@ -83,7 +89,7 @@ const Contact = () => {
                   className=" focus:border-b-2 focus:outline-none  bg-transparent text-sm border-b-2 py-2 placeholder:text-white"
                   placeholder="Your message here"
                 />
-                <button className="bg-white text-purple-600 font-bold px-5 py-2 hover:bg-purple-600 hover:text-white transition-all duration-100 delay-75">
+                <button className="bg-white text-purple-600 font-bold px-2 lg:px-4 lg:py-2 py-1 hover:bg-purple-600 hover:text-white transition-all duration-100 delay-75">
                   Submit
                 </button>
               </form>
