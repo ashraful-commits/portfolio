9f086a404e75024e1a0b6c8eb088d46368d1efec
ashraful-commits <ashrfulalam10000@gmail.com>
2023-10-19 01:32:21 +0600

    update protfolio

--- components/Portfolio/Portfolio.tsx
+++ components/Portfolio/Portfolio.tsx
@@ -97,6 +97,16 @@ const Portfolio = () => {
       tools: ["React", "Redux toolkit"],
       type: "e-commerce",
     },
+    {
+      id: 7,
+      name: "Logo",
+      live: "https://logo-project-assignment.vercel.app/",
+      github: "https://github.com/ashraful-commits/LOGO_PROJECT.git",
+      shortdis: "It's social media app.You can share your via with that!",
+      preview: gallary2,
+      tools: ["React", "Firebase"],
+      type: "social",
+    },
   ];
   return (
     <motion.div
